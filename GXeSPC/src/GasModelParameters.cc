c52b4a91c495d1df778a0bb3c0089f470fc4adf9
Church, Eric D <eric.church@pnnl.gov>
2022-11-10 11:24:11 -0700

    Prepare for first push to github, having stripped a couple directories.

--- GXeSPC/src/GasModelParameters.cc
+++
@@ -1,8 +0,0 @@
-#include "GasModelParameters.hh"
-#include "DegradModel.hh"
-#include "GasModelParametersMessenger.hh"
-#include "DetectorConstruction.hh"
-
-GasModelParameters::GasModelParameters(){
-	fMessenger = new GasModelParametersMessenger(this);
-}

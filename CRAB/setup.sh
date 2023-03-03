96ca6de1774c72190abaa7609a47cca8395ff860
Krishan Mistry <kvjmistry@gmail.com>
2023-03-03 17:55:17 -0600

    Tidy the area

--- CRAB/setup.sh
+++ CRAB/setup.sh
@@ -1,24 +1,39 @@
-alias emacs Emacs
+# Set the path to the Geant4 Installation
+export G4INSTALL=/Users/mistryk2/Packages/geant4-v11/geant4-v11.1.0/install;
+export PATH=$G4INSTALL/bin:$PATH;
+export DYLD_LIBRARY_PATH=$G4INSTALL/lib:$DYLD_LIBRARY_PATH;
+export LD_LIBRARY_PATH=$G4INSTALL/lib:$LD_LIBRARY_PATH;
 
-setenv PATH {$PATH}:/Applications/Emacs.app/Contents/MacOS:/Applications/CMake.app/Contents/bin:/usr/local/gfortran/bin
+cd $G4INSTALL/bin; source geant4.sh; cd -;
 
-cd /Users/chur558/geant4.10.07.02-install/bin
-source /Users/chur558/geant4.10.07.02-install/bin/geant4.csh
-cd -
-source /Users/chur558/root_build/bin/thisroot.csh
 
-setenv MARLEY /Users/chur558/marley
-setenv LD_LIBRARY_PATH {$LD_LIBRARY_PATH}:/Users/chur558/marley/build:/usr/local/gfortran/lib
-setenv DYLD_LIBRARY_PATH {$LD_LIBRARY_PATH}:/Users/chur558/marley/build:/usr/local/gfortran/lib
+# Path to ROOT
+export ROOTSYS=$(brew --cellar root)/6.24.04;
+export PATH=$ROOTSYS/bin/:$PATH;
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ROOTSYS/lib;
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$ROOTSYS/lib;
 
-setenv GARFIELD_HOME /Users/chur558/we22903/garfieldpp
-setenv HEED_DATABASE /Users/chur558/we22903/garfieldpp/Heed/heed++/database
-setenv DEGRAD_HOME /Users/chur558/degrad
+# Garfield
+export GARFIELD_INSTALL=/Users/mistryk2/Packages/garfieldpp/install
+export GARFIELD_HOME=/Users/mistryk2/Packages/garfieldpp/
+export CMAKE_PREFIX_PATH=/Users/mistryk2/Packages/garfieldpp/install:$CMAKE_PREFIX_PATH
+export HEED_DATABASE=$GARFIELD_INSTALL/share/Heed/database
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GARFIELD_INSTALL/lib
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$GARFIELD_INSTALL/lib
 
-setenv NEST_INCLUDE_DIRS /Users/chur558/nest//install/include/NEST
 
-source ~/venvs/bin/activate.csh
 
-# to get proper tab completion in csh, it seems.
-set filec
-set autolist
+# DEGRAD
+export DEGRAD_HOME=/Users/mistryk2/Packages/Degrad
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/opt/homebrew/Cellar/gcc/11.3.0_2/lib;
+export PATH=/opt/homebrew/bin:$PATH
+
+# NEST
+export NEST_INCLUDE_DIRS=/Users/mistryk2/Packages/NEST/install/include/NEST
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/Users/mistryk2/Packages/NEST/install/lib
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/Users/mistryk2/Packages/NEST/install/lib;
+
+export CRABPATH=/Users/mistryk2/Packages/GXeTPCSim/gxsim/CRAB/
+
+# Add the crab exe to the path
+export PATH=$CRABPATH/build:$PATH;
